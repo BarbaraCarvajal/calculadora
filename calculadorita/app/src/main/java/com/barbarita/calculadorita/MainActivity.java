d8879a9906da42fc4f76e2c14aaf45b2ea6ab8da
barbaracarvajal <barb.carvajalsaez@gmail.com>
2023-03-17 12:13:42 -0300

    interfaz calculadora

---
+++ calculadorita/app/src/main/java/com/barbarita/calculadorita/MainActivity.java
@@ -0,0 +1,19 @@
+package com.barbarita.calculadorita;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView textViewResultado;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textViewResultado = findViewById(R.id.textViewResultado);
+    }
+}
